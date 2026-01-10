2372b1bca0bde29e39fc657751284c4490b2ba1b
Mustafa Genç <mgenc404@gmail.com>
2026-01-10 17:54:03 +0300

    L5: Channel Refactor

--- main.go
+++ main.go
@@ -7,30 +7,44 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer close(lines)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil && err != io.EOF {
+				break
+			}
+			if strings.Contains(string(data[:n]), "\n") {
+				firstNewline := strings.Index(string(data[:n]), "\n")
+				str += string(data[:firstNewline])
+				lines <- str
+				remaining := data[firstNewline+1 : n]
+				str = string(remaining)
+			} else if err == io.EOF {
+				if len(str)+n > 0 {
+					lines <- str + string(data[:n])
+				}
+				break
+			} else {
+				str += string(data[:n])
+			}
+		}
+	}()
+	return lines
+}
+
 func main() {
 	msgs, err := os.Open("messages.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer msgs.Close()
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := msgs.Read(data)
-		if err != nil && err != io.EOF {
-			break
-		}
-		if strings.Contains(string(data[:n]), "\n") {
-			firstNewline := strings.Index(string(data[:n]), "\n")
-			str += string(data[:firstNewline])
-			fmt.Printf("read: %s\n", str)
-			remaining := data[firstNewline+1 : n]
-			str = string(remaining)
-		} else if err == io.EOF {
-			fmt.Printf("read: %s\n", str+string(data[:n]))
-			break
-		} else {
-			str += string(data[:n])
-		}
+	linesChan := getLinesChannel(msgs)
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
 	}
 }
