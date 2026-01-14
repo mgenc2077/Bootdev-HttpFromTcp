7c89962e11443a7d671338157f1ce1f550d92f7b
Mustafa Genç <mgenc404@gmail.com>
2026-01-14 19:06:32 +0300

    CH4 L3: Parsing a stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -6,8 +6,33 @@ import (
 	"strings"
 )
 
+const (
+	initialized = iota
+	done        = iota
+)
+
 type Request struct {
 	RequestLine RequestLine
+	State       int
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.State == initialized {
+		parsedLine, size, err := parseRequestLine(string(data))
+		if size == 0 && err == nil {
+			return 0, nil
+		} else if err != nil {
+			return 0, err
+		} else {
+			r.RequestLine = parsedLine
+			r.State = done
+			return size, nil
+		}
+	} else if r.State == done {
+		return 0, errors.New("error: trying to read data in a done state")
+	} else {
+		return 0, errors.New("error: unknown state")
+	}
 }
 
 type RequestLine struct {
@@ -16,32 +41,57 @@ type RequestLine struct {
 	Method        string
 }
 
+const bufferSize = 8
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	str, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-	rline, err := parseRequestLine(strings.SplitN(string(str), "\r\n", 2)[0])
-	if err != nil {
-		return nil, err
+	buf := make([]byte, bufferSize, bufferSize)
+	readToIndex := 0
+	req := &Request{State: initialized}
+	for {
+		if req.State == done {
+			break
+		}
+		if readToIndex == len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
+		n, err := reader.Read(buf[readToIndex:])
+		if err == io.EOF {
+			req.State = done
+			break
+		}
+		readToIndex += n
+		parsedBytes, err := req.parse(buf[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+		copy(buf, buf[parsedBytes:readToIndex])
+		readToIndex -= parsedBytes
 	}
 
-	return &Request{RequestLine: rline}, nil
+	return req, nil
 }
 
-func parseRequestLine(line string) (RequestLine, error) {
-	parts := strings.SplitN(line, " ", 3)
-	if len(parts) != 3 {
-		return RequestLine{}, errors.New("invalid request line")
+func parseRequestLine(line string) (RequestLine, int, error) {
+	firstNewline := strings.Index(line, "\r\n")
+	if firstNewline == -1 {
+		return RequestLine{}, 0, nil
 	}
-	//parts[0] != "GET" && parts[0] != "POST" && parts[0] != "OPTIONS"
+	// Slice the line to include only the request line, excluding \r\n and subsequent data
+	requestLineContent := line[:firstNewline]
+	parts := strings.SplitN(requestLineContent, " ", 3)
+	if len(parts) != 3 {
+		return RequestLine{}, 0, errors.New("invalid request line")
+	}
+
 	if !(parts[0] == "GET" || parts[0] == "POST" || parts[0] == "OPTIONS") || !strings.Contains(parts[1], "/") || strings.TrimPrefix(parts[2], "HTTP/") != "1.1" {
-		return RequestLine{}, errors.New("invalid request line")
+		return RequestLine{}, 0, errors.New("invalid request line")
 	} else {
 		return RequestLine{
 			Method:        parts[0],
 			RequestTarget: parts[1],
 			HttpVersion:   strings.TrimPrefix(parts[2], "HTTP/"),
-		}, nil
+		}, firstNewline + 2, nil
 	}
 }
