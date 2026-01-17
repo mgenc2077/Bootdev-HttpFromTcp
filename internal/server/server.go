b76e242c14c9e52e89c472898f0d5a03bff47579
Mustafa Genç <mgenc404@gmail.com>
2026-01-18 00:42:34 +0300

    CH7 L1: Server

---
+++ internal/server/server.go
@@ -0,0 +1,68 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	Listener   net.Listener
+	Connection net.Conn
+	Status     atomic.Bool
+}
+
+// Creates a net.Listener and returns a new Server instance
+func Serve(port int) (*Server, error) {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	s := &Server{
+		Listener: l,
+	}
+	s.Status.Store(true)
+	go s.listen()
+	return s, nil
+}
+
+// Closes the listener and the server
+func (s *Server) Close() error {
+	if err := s.Connection.Close(); err != nil {
+		return err
+	}
+	if err := s.Listener.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Uses a loop to .Accept new connections as they come in, and handles each one in a new goroutine
+func (s *Server) listen() error {
+	for {
+		// Wait for a connection.
+		conn, err := s.Listener.Accept()
+		if err != nil {
+			return err
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go func(c net.Conn) {
+			if s.Status.Load() == true {
+				s.handle(c)
+			}
+		}(conn)
+	}
+}
+
+// Handles a single connection by writing the response and then closing the connection
+func (s *Server) handle(conn net.Conn) {
+	s.Connection = conn
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"\r\n" +
+		"Hello World!\n"))
+	conn.Close()
+}
